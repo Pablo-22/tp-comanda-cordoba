a87b601267d3930cb3a553376e22ba92b384d5ee
Pablo Córdoba <pablo.cordoba@bromus.com.ar>
2022-06-07 20:52:36 -0300

    Eliminación de código innecesario

--- app/index.php
+++ app/index.php
@@ -36,21 +36,12 @@ $app->group('/usuarios', function (RouteCollectorProxy $group) {
     $group->get('/{nombre}', \UsuarioController::class . ':TraerUno');
     $group->post('[/]', \UsuarioController::class . ':CargarUno');
     //$group->post('/{nombre}', \UsuarioController::class . ':BorrarUno');
-    $group->delete('/:id', \UsuarioController::class . ':BorrarUno');
+    $group->delete('/{id}', \UsuarioController::class . ':BorrarUno');
   });
 
-$app->group('/credenciales', function (RouteCollectorProxy $group) {
-  $group->get('[/]', \UsuarioController::class . ':TraerTodos');
-  $group->post('[/]', \UsuarioController::class . ':ChequearUno');
-})->add(\Logger::class . ':VerificarCredenciales');
-
-$app->group('/json', function (RouteCollectorProxy $group) {
-  $group->get('[/]', \UsuarioController::class . ':TraerTodos');
-  $group->post('[/]', \UsuarioController::class . ':ChequearUno');
-})->add(\Logger::class . ':VerificarCredenciales');
 
 $app->get('[/]', function (Request $request, Response $response) {    
-    $response->getBody()->write("Slim Framework 4 PHP");
+    $response->getBody()->write("Córdoba Pablo - TP 'La Comanda'");
     return $response;
 
 });
