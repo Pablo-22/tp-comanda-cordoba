7de0506a2daf25434707501dc058f5732ac3acae
Pablo Córdoba <pablo.cordoba@bromus.com.ar>
2022-06-14 20:00:20 -0300

    Se comienza la implementación de los estados default

--- app/models/Estado.php
+++ app/models/Estado.php
@@ -6,16 +6,38 @@ class Estado
     public $descripcion;
     public $usuarioCreador;
     public $idEntidad;
+	public $entidad;
 
-    public function guardarEstadoMesa()
+    public function guardarEstado()
     {
         $objAccesoDatos = AccesoDatos::obtenerInstancia();
-        $consulta = $objAccesoDatos->prepararConsulta("
-            INSERT INTO estados_mesas (descripcion, idUsuarioCreador, idEntidad) 
-			:descripcion,
-			(SELECT U.id FROM usuarios U WHERE U.nombre = :nombre),
-			:idEntidad
-        ");
+		switch ($entidad) {
+			case 'usuario':
+				$consulta = $objAccesoDatos->prepararConsulta("
+					INSERT INTO estados_usuarios (descripcion, idUsuarioCreador, idEntidad) 
+					:descripcion,
+					(SELECT U.id FROM usuarios U WHERE U.nombre = :nombre),
+					:idEntidad
+				");
+				break;
+			case 'mesa':
+				$consulta = $objAccesoDatos->prepararConsulta("
+					INSERT INTO estados_mesas (descripcion, idUsuarioCreador, idEntidad) 
+					:descripcion,
+					(SELECT U.id FROM usuarios U WHERE U.nombre = :nombre),
+					:idEntidad
+				");
+				break;
+			case 'pedido':
+				$consulta = $objAccesoDatos->prepararConsulta("
+					INSERT INTO estados_pedidos (descripcion, idUsuarioCreador, idEntidad) 
+					:descripcion,
+					(SELECT U.id FROM usuarios U WHERE U.nombre = :nombre),
+					:idEntidad
+				");
+				break;
+		}
+        
 
         $consulta->bindValue(':idEntidad', $this->idEntidad, PDO::PARAM_STR);
         $consulta->bindValue(':capacidad', $this->descripcion);
@@ -24,9 +46,18 @@ class Estado
 		
         return $objAccesoDatos->obtenerUltimoId();
     }
+	
 
 
 	public static function getEstadoDefaultMesa(){
+		return 'Libre';
+	}
+
+	public static function getEstadoDefaultUsuario(){
+		return 'Libre';
+	}
+
+	public static function getEstadoDefaultPedido(){
 		return 'Libre';
 	}
 }
